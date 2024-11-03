0db13c31fdb2bc33c441583308fa1e6962417224
Yosodog <yosodog@gmail.com>
2024-11-03 11:53:11 -0600

    Initial configuration

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+module.exports = {
+    pwApiUrl: process.env.PW_API_URL,
+    pwApiToken: process.env.PW_API_TOKEN,
+    nexusApiUrl: process.env.NEXUS_API_URL,
+    nexusApiToken: process.env.NEXUS_API_URL,
+};
